f3a80b76da6329f9ab428fb17516194500a056be
Manel <walletdans@gmail.com>
2024-06-23 23:21:29 +0200

    last commit

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,42 @@
-console.log('04 Store API')
+require('dotenv').config()
+// async errors
+
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+//middleware
+
+app.use(express.json())
+
+// routes
+
+app.get('/', (req,res) => {
+    res.send('<h1>Store API</h1> <a href="/api/v1/products">Products route</a>')
+})
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+
+
+// Server + db connection 
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
