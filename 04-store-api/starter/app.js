983408321991defd652020c1d4a4f651c3d23373
Manel <walletdans@gmail.com>
2024-06-26 13:29:35 +0200

    last commit

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -5,7 +5,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
-
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
@@ -20,6 +21,9 @@ app.get('/', (req,res) => {
     res.send('<h1>Store API</h1> <a href="/api/v1/products">Products route</a>')
 })
 
+
+app.use('/api/v1/products', productsRouter)
+
 // products route
 
 app.use(notFoundMiddleware)
@@ -31,6 +35,8 @@ app.use(errorMiddleware)
 
 const start = async () => {
     try {
+        //connectDB
+        await connectDB(process.env.MONGO_URI)
         app.listen(PORT, () => {
             console.log(`Server is listening on port ${PORT}`)
         })
