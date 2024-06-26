0272e7c4563fb07ef56d501559a7c12896028bf1
Manel <walletdans@gmail.com>
2024-06-26 20:43:45 +0200

    last commit

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,35 @@
+const { query } = require('express')
+const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req,res) => {
-    throw new Error('testing async errors')
-    res.status(200).json({msg: 'products testing route'})
+    const products = await Product.find({
+        name: "vase table"
+    
+    })
+    res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg: 'products route'})
+    // De esta manera filtramos solamente por featured..
+    const {featured} = req.query
+
+    const queryObject = {}
+
+    // esto es una expresion ternaria en la que su syntax es la siguiente:
+    // queryObject.featured = featured === 'true' ? true : false;
+    // y seria equivalente a if (featured === 'true') {
+    //queryObject.featured = true;
+    // } else {
+    // queryObject.featured = false;
+    // }
+
+    if (featured) {
+        queryObject.featured = feaured === 'true'? true : false
+    }
+
+    const products = await Product.find(req.query)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 
