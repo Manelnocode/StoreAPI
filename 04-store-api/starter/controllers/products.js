36072254156d3dab275e32ebb0b150df72aa352c
Manel <walletdans@gmail.com>
2024-06-29 17:02:21 +0200

    last commit

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -4,7 +4,10 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
     
-    const products = await Product.find({}).select('name price')
+    const products = await Product.find({price:{ $lt: 30 }})
+    .sort('name')
+    .select('name price')
+    
     
     res.status(200).json({products, nbHits: products.length})
 }
@@ -52,7 +55,12 @@ const getAllProducts = async (req,res) => {
         result = result.select(fieldsList)
     }
 
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 1
+    const skip = (page -1) * limit;
 
+    result = result.skip(skip).limit(limit)
+    
     const products = await result
     res.status(200).json({products, nbHits: products.length})
 }
